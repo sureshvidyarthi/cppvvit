3d3d7195180218e2c4410e9faf8bd8cbf7227fb5
vidyarthi <vidyarthi004@gmail.com>
2024-01-15 19:29:29 +0530

    push

---
+++ day1/prog1.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //Header file library
+
+using namespace std;
+int main()
+{
+    // cout << "Hello World \n\n";
+    // cout << "Hii How are you";
+    cout << "Hello... How are you?" << endl;
+    cout << "Hiii.. I'm great!!";
+
+    return 0;
+}
---
+++ day2/prog1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // int number = 10;
+    //  cout << number;
+
+    // int num;
+    // num = 20;
+    // cout << num;
+
+    // int num = 20;
+    // double myNumber = 500.56;
+    // char myLetter = 'D';
+    // string myName = "Vikash";
+    // bool myBoon = true;
+    // cout << num << endl;
+    // cout << myNumber << endl;
+    // cout << myLetter << endl;
+    // cout << myName << endl;
+    // cout << myBoon;
+
+    // Many Variable Dec
+    //  int x = 10;
+    //  int y = 20;
+    //  int z = 30;
+    //  int w = x + y + z;
+
+    // int x = 10, y = 20, z = 30, w = x + y + z;
+    // cout << w;
+
+    // Rules for variable name
+    // 1. letter, digit, _
+    // 2. Number se start nhi kr sakte hai
+    // 3. case sensative hota hai...
+    // Example-- myname and myName are different
+
+    // Constants in CPP
+    // const myNum = 15;
+    // myNum = 20;
+    // cout << myNum;
+
+    // Value as user input
+    //  int x;
+    //  cout << "Enter a number: ";
+    //  cin >> x;
+    //  cout << "Your number is: " << x;
+
+    // int x, y;
+    // int sum;
+    // cout << "Enter 1st Number: ";
+    // cin >> x;
+    // cout << "Enter 2nd Number: ";
+    // cin >> y;
+    // sum = x + y;
+    // cout << "The sum is: " << sum;
+
+    // Operators in CPP
+
+    int x = 12, y = 6;
+    // cout << ++x;
+    // cout << --y;
+
+    cout << x++;
+    cout << x;
+    return 0;
+}
